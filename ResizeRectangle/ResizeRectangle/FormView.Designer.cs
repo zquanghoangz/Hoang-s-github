2c9d5d2858e8505b65f664b8432243497f6de547
zquanghoangz <zquanghoangz@gmail.com>
2016-09-05 23:14:13 +0800

    Draw rectange on the form
    Draw rectange on the form by using mouse up down move

---
+++ ResizeRectangle/ResizeRectangle/FormView.Designer.cs
@@ -0,0 +1,52 @@
+﻿namespace ResizeRectangle
+{
+    partial class FormView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 1077);
+            this.DoubleBuffered = true;
+            this.Name = "FormView";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormView_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.FormView_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormView_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.FormView_MouseUp);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ResizeRectangle/ResizeRectangle/FormView.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResizeRectangle
+{
+    public partial class FormView : Form
+    {
+        private Point _mouseDownPos;
+        private Point _mouseCurrPos;
+        private bool _holdingDown;
+
+        public FormView()
+        {
+            InitializeComponent();
+        }
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.Coral,
+                Min(_mouseDownPos.X , _mouseCurrPos.X),
+                Min(_mouseDownPos.Y , _mouseCurrPos.Y),
+                Math.Abs(_mouseDownPos.X- _mouseCurrPos.X),
+                Math.Abs(_mouseDownPos.Y - _mouseCurrPos.Y));
+        }
+
+        private int Min(int x1, int x2)
+        {
+            return x1 > x2 ? x2 : x1;
+        }
+
+        private void FormView_MouseDown(object sender, MouseEventArgs e)
+        {
+            _mouseDownPos = new Point(e.X,e.Y);
+            _holdingDown = true;
+        }
+
+        private void FormView_MouseUp(object sender, MouseEventArgs e)
+        {
+            _holdingDown = false;
+        }
+
+        private void FormView_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_holdingDown)
+            {
+                _mouseCurrPos = new Point(e.X,e.Y);
+                Invalidate();
+            }
+        }
+    }
+}
