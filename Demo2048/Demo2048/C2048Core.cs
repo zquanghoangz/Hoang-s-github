045461401a312f0c75f8105d204c6038be2f10dc
zquanghoangz <zquanghoangz@gmail.com>
2016-09-05 23:05:09 +0800

    Demo 2048 game in windows form application
    Demo 2048 game in windows form application

---
+++ Demo2048/Demo2048/C2048Core.cs
@@ -0,0 +1,234 @@
+﻿using System;
+
+namespace Demo2048
+{
+    public class C2048Core
+    {
+        #region Fields & Properties
+
+        private readonly Random _rand;
+        private readonly int[,] _tempMatrix;
+        private bool _anyMove;
+        private bool _moveDetected = true;
+        public int[,] Matrix { get; set; }
+        public decimal Score { get; set; }
+
+        #endregion Fields & Properties
+
+        public C2048Core()
+        {
+            _rand = new Random();
+            Score = 0;
+            _tempMatrix = new int[4, 4];
+            InitCoreMatrix();
+        }
+
+        #region Public Methods
+
+        public void AddNewElement()
+        {
+            if (NotAnyZeroValue())
+            {
+                return;
+            }
+
+            do
+            {
+                int nextIndex = _rand.Next(0, 16);
+                int j = nextIndex%4;
+                int i = nextIndex/4;
+
+                if (Matrix[i, j] == 0)
+                {
+                    Matrix[i, j] = NewValue();
+                    break;
+                }
+            } while (true);
+        }
+
+        public bool PullRight()
+        {
+            InitStep();
+
+            while (_moveDetected)
+            {
+                _moveDetected = false;
+           
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Compare2PointsAndUpdateValue(j, 3 - i, j, 2 - i);
+                    }
+                }
+            }
+
+            return _anyMove;
+        }
+
+        public bool PullLeft()
+        {
+            InitStep();
+
+            while (_moveDetected)
+            {
+                _moveDetected = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Compare2PointsAndUpdateValue(j, i, j, i + 1);
+                    }
+                }
+            }
+
+            return _anyMove;
+        }
+
+        public bool PullBottom()
+        {
+            InitStep();
+
+            while (_moveDetected)
+            {
+                _moveDetected = false;
+              
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Compare2PointsAndUpdateValue(3 - i, j, 2 - i, j);
+                    }
+                }
+            }
+
+            return _anyMove;
+        }
+
+        public bool PullTop()
+        {
+            InitStep();
+
+            while (_moveDetected)
+            {
+                _moveDetected = false;
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        Compare2PointsAndUpdateValue(i, j, i + 1, j);
+                    }
+                }
+            }
+
+            return _anyMove;
+        }
+
+        public bool HasANextMove()
+        {
+            if (!NotAnyZeroValue())
+            {
+                return true;
+            }
+
+            //Has any move
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (Matrix[j, i] == Matrix[j, i + 1] || Matrix[i, j] == Matrix[i + 1, j] ||
+                        Matrix[j, 3 - i] == Matrix[j, 2 - i] || Matrix[3 - i, j] == Matrix[2 - i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        #endregion Public Methods
+
+        #region Private Helper Methods
+
+        private void InitStep()
+        {
+            ResetMatrix(_tempMatrix);
+            _moveDetected = true;
+            _anyMove = false;
+        }
+
+        private bool NotAnyZeroValue()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (Matrix[i, j] == 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void InitCoreMatrix()
+        {
+            Matrix = new int[4, 4];
+            ResetMatrix(Matrix);
+
+            AddNewElement();
+            AddNewElement();
+        }
+
+        private int NewValue()
+        {
+            switch (_rand.Next(0, 10)) //10%
+            {
+                case 1:
+                    return 4;
+                default:
+                    return 2;
+            }
+        }
+
+        private void Compare2PointsAndUpdateValue(int x1, int y1, int x2, int y2)
+        {
+            if (_tempMatrix[x1, y1] == 1 || _tempMatrix[x2, y2] == 1)
+            {
+                return;
+            }
+
+            if (Matrix[x1, y1] == 0 && Matrix[x2, y2] != 0)
+            {
+                Matrix[x1, y1] = Matrix[x2, y2];
+                Matrix[x2, y2] = 0;
+
+                _moveDetected = _anyMove = true;
+            }
+            else if (Matrix[x1, y1] != 0 && Matrix[x2, y2] != 0 && Matrix[x1, y1] == Matrix[x2, y2])
+            {
+                Matrix[x1, y1] = Matrix[x1, y1]*2;
+                Matrix[x2, y2] = 0;
+                Score += Matrix[x1, y1];
+
+                _moveDetected = _anyMove = true;
+                _tempMatrix[x1, y1] = 1;
+            }
+        }
+
+        private void ResetMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        #endregion Private Helper Methods
+    }
+}
---
+++ Demo2048/Demo2048/FormView.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Demo2048
+{
+    public partial class FormView : Form
+    {
+        private const int StartX = 50;
+        private const int StartY = 50;
+        private const int SquareSize = 80;
+        private const int LineSize = 8;
+        private readonly C2048Core _c2048Core = new C2048Core();
+        private Dictionary<int, Brush> _brushes;
+
+        public FormView()
+        {
+            InitializeComponent();
+            DefineColorBrushes();
+        }
+
+        private void DefineColorBrushes()
+        {
+            _brushes = new Dictionary<int, Brush>
+            {
+                {2, Brushes.LightYellow},
+                {4, Brushes.LemonChiffon},
+                {8, Brushes.NavajoWhite},
+                {16, Brushes.SandyBrown},
+                {32, Brushes.Orange},
+                {64, Brushes.DarkOrange},
+                {128, Brushes.Peru},
+                {256, Brushes.Brown},
+                {512, Brushes.Brown},
+                {1024, Brushes.Brown},
+                {2048, Brushes.Brown},
+                {4096, Brushes.Brown},
+                {8192, Brushes.Red},
+                {16384, Brushes.DarkRed},
+            };
+        }
+
+
+        private void FormView_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            for (int i = 0; i < 5; i++)
+            {
+                g.DrawLine(new Pen(Brushes.Tan, LineSize), StartX, StartY + i * SquareSize, StartX + 4 * SquareSize,
+                    StartY + i * SquareSize);
+            }
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                g.DrawLine(new Pen(Brushes.Tan, LineSize), StartX + i * SquareSize, StartY, StartX + i * SquareSize,
+                    StartY + 4 * SquareSize);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (_c2048Core.Matrix[i, j] != 0)
+                    {
+                        FillElement(g, _c2048Core.Matrix[i, j], StartX + j * SquareSize + LineSize / 2,
+                            StartY + i * SquareSize + LineSize / 2,
+                            SquareSize - LineSize, SquareSize - LineSize);
+                    }
+                }
+            }
+
+            g.DrawString(_c2048Core.Score.ToString(), new Font("Tahoma", 14), Brushes.Gray, 10, 10);
+        }
+
+        private void FillElement(Graphics g, int number, int x, int y, int w, int h)
+        {
+            g.FillRectangle(_brushes[number], x, y, w, h);
+            g.DrawString(number.ToString(), new Font("Tahoma", 14), Brushes.Gray, x, y);
+        }
+
+        private void FormView_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (!_c2048Core.HasANextMove())
+            {
+                MessageBox.Show("Lose");
+                return;
+            }
+
+            bool anyMove = false;
+            if (e.KeyCode == Keys.Up)
+            {
+                //Move all to up
+                anyMove = anyMove || _c2048Core.PullTop();
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                //Move all to down
+                anyMove = anyMove || _c2048Core.PullBottom();
+            }
+            else if (e.KeyCode == Keys.Left)
+            {
+                //Move all to left
+                anyMove = anyMove || _c2048Core.PullLeft();
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                //Move all to right
+                anyMove = anyMove || _c2048Core.PullRight();
+            }
+
+            if (anyMove)
+            {
+                Invalidate();
+                //Add new number
+                _c2048Core.AddNewElement();
+
+            }
+        }
+    }
+}
